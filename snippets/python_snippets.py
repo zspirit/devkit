cfa9bb0ea32bae9176848c54eb1980b13d9d3533
Zakaria GAFAOUI <zgafaoui@gmail.com>
2025-07-22 21:02:49 +0200

    refactor python_snippets.py

--- snippets/python_snippets.py
+++ snippets/python_snippets.py
@@ -1,13 +1,9 @@
 """python_snippets module."""
 import os
 import logging
-from datetime import datetime
-from typing import Optional
 
 class PythonSnippets:
     def __init__(self):
         pass
     def run(self):
-        return True
-    def validate(self):
         return True
