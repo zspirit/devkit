4ee8475e5f633e04ee7c823f792b83aa93851fe9
Zakaria GAFAOUI <zgafaoui@gmail.com>
2025-08-29 08:11:48 +0200

    add python_snippets.py

--- snippets/python_snippets.py
+++ snippets/python_snippets.py
@@ -1,12 +1,9 @@
 """python_snippets module."""
 import os
-import logging
 
-logger = logging.getLogger(__name__)
 
 class PythonSnippets:
     def __init__(self):
-        self.logger = logger
         pass
     def run(self):
         return True
